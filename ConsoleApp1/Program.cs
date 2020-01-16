d8916fa11913b099de5497ccbc681f2daf0f7656
Tony Collins <tjc@eltham-college.org.uk>
2020-01-16 12:50:44 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Queue q = new Queue(3);
+
+            q.enqueue(1);
+            q.enqueue(2);
+            q.enqueue(3);
+            q.enqueue(4);
+          
+
+
+            Console.WriteLine(q.dequeue());
+            q.enqueue(5);
+            Console.WriteLine(q.dequeue());
+            Console.WriteLine(q.dequeue());
+            Console.WriteLine(q.dequeue());
+            Console.WriteLine(q.dequeue());
+        }
+    }
+}
---
+++ ConsoleApp1/Queue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Queue
+    {
+        private int[] Q;
+        private int front;
+        private int back;
+        private Boolean full;
+   
+
+        public Queue(int s)
+        {
+            Q = new int[s];
+            front = 0;
+            back = 0;
+            full = false;
+
+        }
+        public void enqueue(int value )
+        {
+            if (full == false)
+            {
+                Q[back] = value;
+                back = (back + 1) % Q.Length;
+                if (back == front) full = true;
+            }else Console.WriteLine("Full");
+        }
+
+        public int dequeue()
+        {
+            int value;
+            if (back != front || full == true)
+            {
+                value = Q[front];
+                front = (front + 1) % Q.Length;
+                full = false;
+            }
+            else
+            {
+                value = -999;
+                Console.WriteLine("Q Full");
+            }
+            return value;
+
+        }
+
+
+
+
+    }
+}
